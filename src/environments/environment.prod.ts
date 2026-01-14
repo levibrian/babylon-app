1a3b26456a5c06193fd6b94996c8c4f08531c079
Brian Levi <brianlevi@Brians-MacBook-Air.local>
2026-01-14 13:19:31 +0100

    feat: added production environment config

---
+++ src/environments/environment.prod.ts
@@ -0,0 +1,5 @@
+export const environment = {
+  production: true,
+  apiUrl: 'https://YOUR_PRODUCTION_API_URL_HERE',
+  googleClientId: '68098676036-m5jf61epa39rmo82d9ili5uvfvsmm023.apps.googleusercontent.com'
+};
