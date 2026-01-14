3b1c8c1180272255a3867c788ea51759d024956f
Brian Levi <brianlevi@Brians-MacBook-Air.local>
2026-01-14 17:17:26 +0100

    updated production url

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,5 +1,5 @@
 export const environment = {
   production: true,
-  apiUrl: 'https://YOUR_PRODUCTION_API_URL_HERE',
+  apiUrl: 'https://babylon-api.fly.dev',
   googleClientId: '68098676036-m5jf61epa39rmo82d9ili5uvfvsmm023.apps.googleusercontent.com'
 };
